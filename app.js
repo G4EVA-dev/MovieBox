206931cdd3ecc07d51a1d2228c5d4bfcb73bd126
G4EVA-dev <tanzeglenn@gmail.com>
2023-09-14 03:49:25 +0100

    api up running

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+//!GLOBAL VARIABLES
 const apiKey = "9fa6930b930e84f99ee4b7654fb3e0f9";
 const apiUrl = "https://api.themoviedb.org/3/movie/";
-// const apiUrl = "https://api.themoviedb.org/3/movie/550?";    
+const searchInput = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
 
 async function searchMovie(apID) {
-  const response = await fetch(apiUrl + apID + '?' +  `api_key=${apiKey}`);
+  const response = await fetch(apiUrl + apID + "?" + `api_key=${apiKey}`);
   var data = await response.json();
 
+  //!LOCAL VARIABLES
   let releaseDate = data.release_date;
   let runtime = data.runtime;
   let title = data.title;
@@ -14,10 +17,17 @@ async function searchMovie(apID) {
   let scrUrl = "https://image.tmdb.org/t/p/original";
   let poster = scrUrl + posterPath;
 
-  document.querySelector("#movieTitle").innerHTML = data.title;
-  document.querySelector("#movieDate").innerHTML = data.release_date;
-  document.querySelector("#moviePoster").innerHTML = poster;
-  document.querySelector("#runtime").innerHTML = runtime;
-  console.log(data);
+  if (response.status === 404) {
+    alert("Error");
+  } else {
+    document.querySelector("#movieTitle").innerHTML = data.title;
+    document.querySelector("#movieDate").innerHTML = data.release_date;
+    document.querySelector("#moviePoster").innerHTML = poster;
+    document.querySelector("#runtime").innerHTML = runtime;
+  }
+
+  // console.log(data);
 }
-searchMovie(17);
+searchButton.addEventListener("click", () => {
+  searchMovie(searchInput.value);
+});
